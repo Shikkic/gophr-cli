1fc333644b909018dbc0a8b209c9e1e32d4bf658
Dan Cadden <shikkic@gmail.com>
2016-03-30 00:27:19 -0400

    remove init functionality from gophr.go

--- gophr.go
+++ gophr.go
@@ -173,29 +173,7 @@ func main() {
 					projectName = strings.Replace(projectNameInput, string('\n'), "", 1)
 				}
 
-				initPath := filepath.Join(goPath, "src", "github.com", repoAuthor, projectName)
-				os.MkdirAll(initPath, 0777)
-
-				// Now we need to glob to make sure a file name like that doesn't already exists
-				fls, err := filepath.Glob(initPath + "/*.go")
-				check(err)
-				fmt.Println(fls)
-
-				if len(fls) > 0 {
-					// check if the .go file names match your project name
-					for _, fileName := range fls {
-						if fileName == initPath+"/"+projectName+".go" {
-							red := color.New(color.FgRed).SprintFunc()
-							magenta := color.New(color.FgMagenta).SprintFunc()
-							fmt.Printf("%s gophr %s %s file with that name already exists\n", red("✗"), red("ERROR"), magenta("init"))
-							os.Exit(3)
-						}
-					}
-				}
-
-				newFileBuffer := []byte(basicSkeleton)
-				err = ioutil.WriteFile(filepath.Join(initPath, projectName)+".go", newFileBuffer, 0644)
-				check(err)
+				RunInitCommand(goPath, repoAuthor, projectName)
 			},
 		},
 		{
