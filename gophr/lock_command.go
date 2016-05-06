41b27477ae9ea064f389123f4d01b235d309829b
Shikkic <Shikkic@gmail.com>
2016-05-06 01:48:35 -0400

    removed depricated code + lock command should be almost working

--- gophr/lock_command.go
+++ gophr/lock_command.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
 
@@ -49,6 +50,22 @@ func RunLockCommand(fileName string, c *cli.Context) {
 	// Insert new packages into import statements
 	replaceVersionedPackages(file, fileName, versionedPackageURLs)
 
+	// Run go fmt on file
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	s.Start()
+	cmd := exec.Command("go", "fmt", fileName)
+	err = cmd.Run()
+	s.Stop()
+	Check(err)
+
+	// Run go get and install the packages
+	s.Start()
+	fmt.Println("preparing to run go get")
+	cmd = exec.Command("go", "get", "--insecure")
+	err = cmd.Run()
+	s.Stop()
+	Check(err)
+
 	// Print out new packages for go file(s)
 	ReadFile(fileName)
 }
@@ -72,13 +89,11 @@ func replaceVersionedPackages(file io.Reader, fileName string, versionedPackages
 					depName := strings.Replace(scanner.Text(), string('"'), "", 2)
 					depName = strings.Replace(depName, string('\t'), "", 1)
 					for _, packageURL := range versionedPackages {
-						packageURLTokens := strings.Split(string(packageURL), "/")
-						author := packageURLTokens[1]
-						repo := packageURLTokens[2]
-						fmt.Println(string(packageURL))
-						if strings.Contains(depName, author+"/"+repo) {
+						depNameTokens := strings.Split(depName, "/")
+						author := depNameTokens[1]
+						repo := depNameTokens[2]
+						if strings.Contains(string(packageURL), author+"/"+repo) {
 							byteBuffer = packageURL
-							fmt.Println(string(byteBuffer))
 						}
 					}
 				}
@@ -208,60 +223,3 @@ func buildVersionDTO(data []byte, versionStruct *[]common.VersionDTO) {
 	err := ffjson.Unmarshal(data, &versionStruct)
 	Check(err)
 }
-
-////////////////
-// DEPRICATED //
-////////////////
-/*
-func RunLock(fileName string) {
-	// If a dep exist begin process of removing it from the import statement
-	file, err := os.Open("./" + fileName)
-	foundImport := false
-	newFileBuffer := make([]byte, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fileLine := scanner.Text() + "\n"
-		byteBuffer := scanner.Bytes()
-
-		if foundImport == true {
-			if fileLine == ")\n" {
-				foundImport = false
-			} else {
-				// TODO ask for a version number
-				if strings.Contains(fileLine, "github.com") {
-					Magenta := color.New(color.FgMagenta).SprintFunc()
-					reader := bufio.NewReader(os.Stdin)
-					depName := strings.Replace(scanner.Text(), string('"'), "", 2)
-					depName = strings.Replace(depName, string('\t'), "", 1)
-					versionList := retrieveVersionList(depName)
-					fmt.Printf("%s \n", Magenta(depName))
-					fmt.Println(versionList)
-					fmt.Print("version number:")
-					versionNumberInput, _ := reader.ReadString('\n')
-					versionNumber := strings.Replace(versionNumberInput, string('\n'), "", 1)
-					byteBuffer = []byte("\t\"" + strings.Replace(depName, "github.com/", "gophr.dev/", 1) + "@" + versionNumber + "\"")
-				}
-			}
-		} else if fileLine == "import (\n" {
-			foundImport = true
-		}
-
-		byteBuffer = append(byteBuffer, byte('\n'))
-		for _, token := range byteBuffer {
-			newFileBuffer = append(newFileBuffer, token)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	err = ioutil.WriteFile("./"+fileName, newFileBuffer, 0644)
-	Check(err)
-}
-*/
